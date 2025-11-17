fc144f1f4311f7675f6eeb102a3a62666c2b821c
moshahidraza1 <codinghandle@gmail.com>
2025-11-17 20:28:57 +0530

    added:header,hero and portfolio section

---
+++ app/components/Services.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div id='services' className='bg-cream py-20 md:py-28'>
+      <div className='text-center text-4xl md:text-5xl text-dark/90'>
+        Our Expertise in Crafting Spaces
+      </div>
+    </div>
+  )
+}
+
+export default Services
